699be9b47022adca0e2c58639740cdab4125c16b
Scott Robb <scottrobb10@gmail.com>
2022-08-31 13:03:18 +0100

    Added queries + simple css

--- inventory-app/queries.js
+++ inventory-app/queries.js
@@ -1,4 +1,6 @@
+const { response } = require('express')
 const { db } = require('../server/routes/db')
+//import React, { useState, useEffect } from 'react';
 
 //Returns all data in the database
 async function allData(){
@@ -7,14 +9,14 @@ async function allData(){
     return data
 } //allData()
 
-//Use 'id' to state the row
+//Use 'id' to state the id of the row of data you want returned
 async function oneRowData(id){
     const data = await db.query('SELECT * FROM items WHERE id =' + '"' + id + '"')
     console.log(data)
     return data
 } //oneRowData(2)
 
-//Use 'column' to state the column name (e.g. 'title') and 'id' for the row
+//Use 'column' to state the column name (e.g. 'title') and 'id' for the row to be returned
 async function oneSpecificData(column, id){
     const data = await db.query('SELECT ' + '"' + column + '"' + ' FROM items WHERE id =' + '"' + id + '"')
     console.log(data)
